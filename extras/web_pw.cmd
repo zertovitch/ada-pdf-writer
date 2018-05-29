084407cda080e203093771e8c77d5dcd46e4dceb
gdemont <gdemont@c5077b39-e1aa-4ac7-bd0e-390e41f52197>
2018-05-29 12:29:55 +0000

    (extras) Update of gnathtml script for source browsing

--- extras/web_pw.cmd
+++ extras/web_pw.cmd
@@ -10,12 +10,18 @@ echo end Small_demo;>>small_demo.adb
 
 rem Call GNATMake without project file: we want the .ali here.
 
-gnatmake ..\demo\pdf_out_demo.adb -I.. -I..\gid -I..\demo -j3
-gnatmake small_demo.adb -I.. -I..\gid -j3
+gnatmake ..\demo\pdf_out_demo.adb    -I.. -I..\gid -I..\demo -I..\test -I..\tools -j0
+gnatmake ..\test\page_test.adb       -I.. -I..\gid -I..\demo -I..\test -I..\tools -j0
+gnatmake ..\test\validation_test.adb -I.. -I..\gid -I..\demo -I..\test -I..\tools -j0
+gnatmake ..\tools\img2pdf.adb        -I.. -I..\gid -I..\demo -I..\test -I..\tools -j0
+
+gnatmake small_demo.adb -I.. -I..\gid -j0
 
 rem Small_Demo without local references
-perl pw_html.pl small_demo -d -I.. -I..\gid -opw_html
-perl pw_html.pl pdf_out_demo pdf_out.ads pdf_out.adb -I.. -I..\gid -I..\demo -f -d -opw_html
+perl pw_html.pl     small_demo -d -I.. -I..\gid -opw_html
+rem The rest with local references
+perl pw_html.pl     pdf_out_demo pdf_out.ads pdf_out.adb img2pdf.adb page_test.adb validation_test.adb -I.. -I..\gid -I..\demo -I..\test -I..\tools -f -d -opw_html
 
 del *.ali
 del *.o
+del *.exe
